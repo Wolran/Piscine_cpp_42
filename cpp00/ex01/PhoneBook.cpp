e1184cb861bac8df7976ff7f6050243e3c0847ed
Wolran <38337827+Wolran@users.noreply.github.com>
2023-03-18 04:16:46 +0100

    Add files via upload

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: vmuller <vmuller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 05:42:50 by vmuller           #+#    #+#             */
-/*   Updated: 2023/03/18 03:50:37 by vmuller          ###   ########.fr       */
+/*   Updated: 2023/03/18 04:15:00 by vmuller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,34 +24,37 @@ int main(void)
 	int i = 0;
 	PhoneBook P;
 	P.nombre = 0;
+	int flag = 0;
 	std::cout << "les commandes possibles sont : ADD / SEARCH / EXIT" << std::endl;
 	while (1)
 	{
 		i = i % 8;
+		std::cout << "Phone > ";
 		std::getline(std::cin, str);
 		if_exit(str);
 		if (str == "ADD")
 		{
-			std::cout << "nom: "; 
+			std::cout << "fist name: "; 
 			std::getline(std::cin, str);
 			P.repertoire[i].nom = str;
-			std::cout << "prenom: "; 
+			std::cout << "last name: "; 
 			std::getline(std::cin, str);
 			P.repertoire[i].prenom = str;
-			std::cout << "surnom: "; 
+			std::cout << "nickname: "; 
 			std::getline(std::cin, str);
 			P.repertoire[i].surnom = str;
 			std::cout << "phone number: "; 
 			std::getline(std::cin, str);
 			P.repertoire[i].phone = str;
-			std::cout << "secret: ";
+			std::cout << "darkest secret: ";
 			std::getline(std::cin, str);
 			P.repertoire[i].secret = str;
-			std::cout << "le contact " << P.repertoire[i++].surnom << " a bien été créer" << std::endl;
+			std::cout << "le contact " << P.repertoire[i++].surnom << " à bien été enregistré" << std::endl;
 			if (P.nombre < 8)
 				P.nombre++;
+			flag = 1;
 		}
-		else if (str == "SEARCH")
+		else if (str == "SEARCH" && flag != 0)
 		{
 			for (int j = 0; j < P.nombre; j++)
 			{
@@ -70,6 +73,7 @@ int main(void)
 					std::cout << "|" << std::setw(10) << P.repertoire[j].surnom << "|" << std::endl;
 			}
 			std::cout << "veuillez choisir le numéro du contact dans le tableau" << std::endl;
+			std::cout << "Phone > ";
 			std::getline(std::cin, str);
 			if_exit(str);
 			int len = std::atoi(str.c_str());
@@ -77,16 +81,19 @@ int main(void)
 			{
 				std::cout << "ERROR : veuillez choisir le numéro du contact dans le tableau" << std::endl;
 				if_exit(str);
+				std::cout << "Phone > ";
 				std::getline(std::cin, str);
 				len = std::atoi(str.c_str());
 			}
-			std::cout << "nom : " << P.repertoire[len - 1].nom << std::endl;
-			std::cout << "prenom : " << P.repertoire[len - 1].prenom << std::endl;
-			std::cout << "surnom : " << P.repertoire[len - 1].surnom << std::endl;
-			std::cout << "phone : " << P.repertoire[len - 1].phone << std::endl;
-			std::cout << "secret : " << P.repertoire[len - 1].secret << std::endl;
+			std::cout << "first name: " << P.repertoire[len - 1].nom << std::endl;
+			std::cout << "last name: " << P.repertoire[len - 1].prenom << std::endl;
+			std::cout << "nickname: " << P.repertoire[len - 1].surnom << std::endl;
+			std::cout << "phone number: " << P.repertoire[len - 1].phone << std::endl;
+			std::cout << "darkest secret: " << P.repertoire[len - 1].secret << std::endl;
 		}
-		else 
+		else if (str == "SEARCH" && flag == 0)
+			std::cout << "ERROR : liste de contact vide" << std::endl;
+		else
 			std::cout << "les commandes possibles sont : ADD / SEARCH / EXIT" << std::endl;
 	}
 	return 0;
